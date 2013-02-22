a6ce4b79132b36e26646e28ec0d11fc52018bb63
ebifrier <ebifrier@hotmail.co.jp>
2013-02-23 01:35:07 +0900

    VoteProtocol/ServerSettings.cs: 通信プロトコルのバージョンを更新しました。 dist/history.yaml: 更新履歴を更新しました。

--- VoteProtocol/ServerSettings.cs
+++ VoteProtocol/ServerSettings.cs
@@ -17,7 +17,7 @@ namespace VoteSystem.Protocol
         /// 通信プロトコルのバージョンです。
         /// </summary>
         public static readonly PbProtocolVersion ProtocolVersion =
-            new PbProtocolVersion(2, 1, 6);
+            new PbProtocolVersion(2, 1, 7);
 
         /// <summary>
         /// 投票サーバーのアドレスです。
