a0e7eda89eb6d9cc121eef6a335dfbec0d930b02
ebifrier <ebifrier@hotmail.co.jp>
2013-05-11 12:53:13 +0900

    dist/history.yaml: 更新履歴を更新しました。 バージョンを1.3.1に更新しました。

--- PluginShogi/ShogiPlugin.cs
+++ PluginShogi/ShogiPlugin.cs
@@ -195,10 +195,10 @@ namespace VoteSystem.PluginShogi
                 ShogiGlobal.MainWindow = null;
 
             ShogiGlobal.MainWindow = window;
-            //window.Show();
+            window.Show();
 
-            var xx = new View.ShogiEndRollControl();
-            xx.Show();
+            //var xx = new View.ShogiEndRollControl();
+            //xx.Show();
         }
 
         /// <summary>
